531a79a20ae266e300322815cc464eebf1b4fd95
segfault0x61 <torresj1997@outlook.com>
2023-03-27 10:32:27 -0500

    feat: user list fetching using anilist ID

--- YukinaBot/Modules/ListModule.cs
+++ YukinaBot/Modules/ListModule.cs
@@ -5,10 +5,18 @@ namespace YukinaBot.Modules
     public class ListModule : AbstractModule
     {
         [Command("user")]
-        [Summary("Gets statistics about a user.")]
+        [Summary("Gets statistics about a user using their username.")]
         public async Task GetAnimeListAsync([Remainder] string userName)
         {
             var userResponse = await AniListFetcher.FindUserStatisticsAsync(userName);
+            await ReplyAsync("", false, EmbedUtility.BuildUserEmbed(userResponse.User));
+        }
+
+        [Command("user")]
+        [Summary("Gets statistics about a user using their AniList ID.")]
+        public async Task GetAnimeListAsync([Remainder] int userId)
+        {
+            var userResponse = await AniListFetcher.FindUserStatisticsAsync(userId);
             await ReplyAsync("", false, EmbedUtility.BuildUserEmbed(userResponse.User));
         }
     }
